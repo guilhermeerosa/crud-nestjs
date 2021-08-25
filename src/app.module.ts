97870b0a71619744b924057b1cfdf2d324b694b8
Guilherme Rosa <guilhermeerosa@hotmail.com>
2021-08-25 00:09:06 -0300

    crud user

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
